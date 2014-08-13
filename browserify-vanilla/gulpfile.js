5eaf2095ec4fc1a2327b5cb29d14603c05a7cd36
Hafiz Ismail <sgk.sprm@gmail.com>
2014-08-13 11:19:22 +0800

    Updated ```browserify-vanilla``` after feedback from @adam-lynch (https://github.com/deepak1556/gulp-browserify/issues/75#issuecomment-51966740)

--- browserify-vanilla/gulpfile.js
+++ browserify-vanilla/gulpfile.js
@@ -3,34 +3,47 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
 
-gulp.task('browserify', function (cb) {
+gulp.task('browserify', function () {
 
   // create an instance of browserify
   // in this example, we are telling it to look at src/main.js and have it walk through our require() dependencies
   // and bundle those in one file
   // for more browserify options, https://github.com/substack/node-browserify#var-b--browserifyfiles-or-opts
-  var b = browserify('./src/main.js', {});
+  var b = browserify('./src/*.js', {});
 
   // instead of specifying the file(s) we want to bundle in the constructor,
   // we can use b.add(filename) instead.
 
-  // bundle() returns a readable stream that we can use to start piping
-  var bundle = b.bundle();
+  // bundle() returns a regular NodeJS Readable stream that we can use to start piping
+  var stream =  b.bundle()
 
-  // create a new file stream with the output filename of our bundled .js
-  var stream = bundle.pipe(source('bundle.js'));
+    // since gulp itself and its plugins work on vinyl file objects instead of regular streams,
+    // we will use vinyl-source-stream to convert the regular stream from b.bundle()
+    // to a streaming vinyl file object
+    .pipe(source('bundle.js'))
 
-  // add more to the stream pipeline here if needed
-  // minify, uglify, transform etc
+    // add more transformation tasks to the pipeline here if needed,
+    // for eg: minify, uglify, transform etc.
+    // by default, most gulp plugins (especially for transformation tasks)
+    // works on buffered vinyl file object (for eg: uglify).
+    // if needed, you may use vinyl-buffer to convert the streaming vinyl file object
+    // to a buffered vinyl file object
 
-  // destination folder that we will write bundle.js to
-  stream.pipe(gulp.dest('./dist'));
+    // an example transformation
+    .pipe(buffer())
+    .pipe(uglify())
 
-  // return stream if we want to make is an asynchronous task
+    // destination folder that we will write bundle.js to
+    .pipe(gulp.dest('./dist'));
+
+  // return stream if we want to make this an asynchronous task
   return stream;
 
 });
+
 
 gulp.task('default', ['browserify']);
 
